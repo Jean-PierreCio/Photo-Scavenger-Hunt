3a65df9c0a689119ad19bad981274ef9aa1d061e
Jean-Pierre Ciotta <jpciotta@gmail.com>
2023-02-12 23:37:44 -0500

    fin

---
+++ lab-task-squirrel/Models/Task.swift
@@ -0,0 +1,42 @@
+//
+//  Task.swift
+//  lab-task-squirrel
+//
+//  Created by Charlie Hieger on 11/15/22.
+//
+
+import UIKit
+import CoreLocation
+
+class Task {
+    let title: String
+    let description: String
+    var image: UIImage?
+    var imageLocation: CLLocation?
+    var isComplete: Bool {
+        image != nil
+    }
+
+    init(title: String, description: String) {
+        self.title = title
+        self.description = description
+    }
+
+    func set(_ image: UIImage, with location: CLLocation) {
+        self.image = image
+        self.imageLocation = location
+    }
+}
+
+extension Task {
+    static var mockedTasks: [Task] {
+        return [
+            Task(title: "Find a statue",
+                 description: "It has to be famous"),
+            Task(title: "Find a 4 leaf clover",
+                 description: "no cheating"),
+            Task(title: "Find a waterfall",
+                 description: "if you want take a shower in the waterfall")
+        ]
+    }
+}
